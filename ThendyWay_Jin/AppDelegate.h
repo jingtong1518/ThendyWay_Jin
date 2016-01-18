6dc68cedcbc5201919320c1756115687b92f7079
jingtong1518 <qianfeng@youdu.local>
2016-01-18 19:12:59 +0800

    Initial Commit

---
+++ ThendyWay_Jin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThendyWay_Jin
+//
+//  Created by qianfeng on 16/1/18.
+//  Copyright © 2016年 景通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThendyWay_Jin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThendyWay_Jin
+//
+//  Created by qianfeng on 16/1/18.
+//  Copyright © 2016年 景通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
